19c46e95b9f076baa7e8a57e1d90312a268b7d34
Julia <juliagalimidi@knights.ucf.edu>
2022-01-29 15:47:06 -0500

    gem stuff

---
+++ Assets/Game_Manager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public int Timer;
+    public int FirePotatoTimer;
+    public int IcePotatoTimer;
+    
+    
+}
