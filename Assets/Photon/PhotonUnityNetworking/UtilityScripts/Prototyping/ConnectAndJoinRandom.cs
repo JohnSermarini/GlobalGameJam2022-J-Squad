929ce8dc9835d2a11de69f92ed8e906c6bea7016
Julia <juliagalimidi@knights.ucf.edu>
2022-01-29 12:08:56 -0500

    spawn points

--- Assets/Photon/PhotonUnityNetworking/UtilityScripts/Prototyping/ConnectAndJoinRandom.cs
+++ Assets/Photon/PhotonUnityNetworking/UtilityScripts/Prototyping/ConnectAndJoinRandom.cs
@@ -27,6 +27,7 @@ namespace Photon.Pun.UtilityScripts
     public class ConnectAndJoinRandom : MonoBehaviourPunCallbacks
     {
         public GameObject MyWizard;
+        public GameObject XROrigin;
 
         //SpawnPoints
         public Transform FireWizTransform;
@@ -40,7 +41,6 @@ namespace Photon.Pun.UtilityScripts
         /// <summary>Used as PhotonNetwork.GameVersion.</summary>
         public byte Version = 1;
 
-        /// <summary>Max number of players allowed in room. Once full, a new room will be created by the next connection attemping to join.</summary>
         [Tooltip("The max number of players allowed in room. Once full, a new room will be created by the next connection attemping to join.")]
         public byte MaxPlayers = 4;
 
@@ -51,6 +51,7 @@ namespace Photon.Pun.UtilityScripts
             FireWizTransform = GameObject.Find("FireWizardSpawnPoint").GetComponent<Transform>();
             IceWizTransform = GameObject.Find("IceWizardSpawnPoint").GetComponent<Transform>();
             ThirdWizTransform = GameObject.Find("ThirdWizard").GetComponent<Transform>();
+            XROrigin = GameObject.Find("XR Origin");
         }
 
         public void Start()
@@ -63,29 +64,18 @@ namespace Photon.Pun.UtilityScripts
 
         public void ConnectNow()
         {
-            Debug.Log("ConnectAndJoinRandom.ConnectNow() will now call: PhotonNetwork.ConnectUsingSettings().");
-
-
             PhotonNetwork.ConnectUsingSettings();
             PhotonNetwork.GameVersion = this.Version + "." + SceneManagerHelper.ActiveSceneBuildIndex;
 
         }
 
-
-        // below, we implement some callbacks of the Photon Realtime API.
-        // Being a MonoBehaviourPunCallbacks means, we can override the few methods which are needed here.
-
-
         public override void OnConnectedToMaster()
         {
-            Debug.Log("OnConnectedToMaster() was called by PUN. This client is now connected to Master Server in region [" + PhotonNetwork.CloudRegion +
-                "] and can join a room. Calling: PhotonNetwork.JoinRandomRoom();");
             PhotonNetwork.JoinRandomRoom();
         }
 
         public override void OnJoinedLobby()
         {
-            Debug.Log("OnJoinedLobby(). This client is now connected to Relay in region [" + PhotonNetwork.CloudRegion + "]. This script now calls: PhotonNetwork.JoinRandomRoom();");
             PhotonNetwork.JoinRandomRoom();
         }
 
@@ -100,16 +90,10 @@ namespace Photon.Pun.UtilityScripts
             PhotonNetwork.CreateRoom(null, roomOptions, null);
         }
 
-        // the following methods are implemented to give you some context. re-implement them as needed.
-        public override void OnDisconnected(DisconnectCause cause)
-        {
-            Debug.Log("OnDisconnected(" + cause + ")");
-        }
 
         public override void OnJoinedRoom()
         {
             base.OnJoinedRoom();
-
             CreatePlayer();
         }
 
@@ -122,25 +106,26 @@ namespace Photon.Pun.UtilityScripts
         public void CreatePlayer()
         {
             int playerCount = PhotonNetwork.PlayerList.Length;
-            Debug.Log(playerCount);
             if(playerCount == 1)
             {
                 this.transform.position = IceWizTransform.position;
                 this.transform.rotation = IceWizTransform.rotation;
-                MyWizard = PhotonNetwork.Instantiate("IceWizard", IceWizTransform.transform.position, IceWizTransform.transform.rotation);
+                MyWizard = PhotonNetwork.Instantiate("IceWizard", transform.position, transform.rotation);
             }
             else if(playerCount == 2)
             {
                 this.transform.position = FireWizTransform.position;
                 this.transform.rotation = FireWizTransform.rotation;
-                MyWizard = PhotonNetwork.Instantiate("FireWizard", FireWizTransform.transform.position, IceWizTransform.transform.rotation);
+                MyWizard = PhotonNetwork.Instantiate("FireWizard", transform.position, transform.rotation);
             }
             else
             {
                 this.transform.position = ThirdWizTransform.position;
                 this.transform.rotation = ThirdWizTransform.rotation;
-                MyWizard = PhotonNetwork.Instantiate("FireWizard", ThirdWizTransform.transform.position, ThirdWizTransform.transform.rotation);
+                MyWizard = PhotonNetwork.Instantiate("FireWizard", transform.position, transform.rotation);
             }
+            XROrigin.transform.position = this.transform.position;
+            XROrigin.transform.rotation = this.transform.rotation;
         }
     }
 }
